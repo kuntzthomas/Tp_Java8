5da1fe05b1494441a54987abf2f3c5bd4cfad08f
Thomas Kuntz <Thomas.kuntz68@gmail.com>
2017-07-10 11:14:44 +0200

    Ex1

--- src/test/java/java8/ex01/Method_01_Test.java
+++ src/test/java/java8/ex01/Method_01_Test.java
@@ -17,8 +17,15 @@ public class Method_01_Test {
     interface IDao {
         List<Person> findAll();
 
-        // TODO créer une méthode int sumAge()
-        // TODO Cette méthode retourne le résultat de l'addition des ages des personnes
+        default int sumAge(){
+        	int sommeAge = 0;
+   
+			for (Person p : findAll()) {
+				sommeAge += p.getAge();
+			}
+        	return sommeAge;
+        }
+
     }
     // end::IDao[]
 
@@ -47,8 +54,7 @@ public class Method_01_Test {
 
         DaoA daoA = new DaoA();
 
-        // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoA.sumAge();
 
         assert result == 210;
     }
@@ -58,8 +64,7 @@ public class Method_01_Test {
 
         DaoB daoB = new DaoB();
 
-        // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoB.sumAge();
 
         assert result == 5050;
 
